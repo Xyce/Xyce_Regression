b237a64f5b63f7006a3e5b8625ccf5cb476b9456
Pete Sholander <peshola@sandia.gov>
2019-09-09 11:58:59 -0600

    Test FORMAT=TECPLOT for .PRINT PCE and .PRINT ES
    This commit adds tests for .PRINT ES and .PRINT PCE for
    FORMAT=TECPLOT.
    
    SON Bugs 1201 and 1231
    http://joseki-vm.sandia.gov/bugzilla/show_bug.cgi?id=1201
    http://joseki-vm.sandia.gov/bugzilla/show_bug.cgi?id=1231

---
+++ Netlists/Output/ES/es-proj-pce-dc-tecplot.cir.sh
@@ -0,0 +1,76 @@
+#!/usr/bin/env perl
+
+# The input arguments to this script are:
+# $ARGV[0] = location of Xyce binary
+# $ARGV[1] = location of xyce_verify.pl script
+# $ARGV[2] = location of compare script
+# $ARGV[3] = location of circuit file to test
+# $ARGV[4] = location of gold standard prn file
+
+# If Xyce does not produce a prn file, then we return exit code 10.
+# If Xyce succeeds, but the test fails, then we return exit code 2.
+# If the shell script fails for some reason, then we return exit code 1.
+# Otherwise we return the exit code of compare or xyce_verify.pl
+
+# Since the shell script runs Xyce also, it is responsible for capturing any
+# error output from Xyce.  The script run_xyce_regression captures the test
+# output and handles the resulting files.
+
+$XYCE=$ARGV[0];
+$XYCE_VERIFY=$ARGV[1];
+#$XYCE_COMPARE=$ARGV[2];
+$CIRFILE=$ARGV[3];
+$GOLDPRN=$ARGV[4];
+
+# remove old files if they exist
+`rm -f $CIRFILE.ES.* $CIRFILE.err $CIRFILE.out $CIRFILE.trim $CIRFILE.goldtrim`;
+
+# run Xyce
+$CMD="$XYCE $CIRFILE > $CIRFILE.out 2> $CIRFILE.err";
+$retval = system("$CMD");
+if ($retval != 0)
+{
+  if ($retval & 127)
+  {
+    print "Exit code = 13\n";
+    printf STDERR "Xyce crashed with signal %d on file %s\n",($retval&127),$CIRFILE;
+    exit 13;
+  }
+  else
+  {
+    print "Exit code = 10\n";
+    printf STDERR "Xyce exited with exit code %d on %s\n",$retval>>8,$CIRFILE;
+    exit 10;
+  }
+}
+
+# Exit if the ES.dat file was not made
+if (not -s "$CIRFILE.ES.dat" )
+{
+  print "$CIRFILE.ES.dat file is missing\n";
+  print "Exit code = 14\n";
+  exit 14;
+}
+
+# Now check the .ES.dat file
+$absTol=1e-5;
+$relTol=1e-3;
+$zeroTol=1e-10;
+
+# must remove the data line from the gold file and test file
+$fc = $XYCE_VERIFY;
+$fc=~ s/xyce_verify/file_compare/;
+$GOLDES = substr($GOLDPRN,0,-3)."ES.dat";
+
+`grep -v "TIME" $CIRFILE.ES.dat > $CIRFILE.trim`;
+`grep -v "TIME" $GOLDES > $CIRFILE.goldtrim`;
+
+$retcode=0;
+$CMD="$fc $CIRFILE.trim $CIRFILE.goldtrim $absTol $relTol $zeroTol > $CIRFILE.ES.out 2> $CIRFILE.ES.err";
+$retval = system("$CMD");
+if ($retval != 0){
+  print STDERR "Comparator exited with exit code $retval on file $CIRFILE.ES.dat\n";
+  $retcode = 2;
+}
+
+print "Exit code = $retcode\n"; exit $retcode;
