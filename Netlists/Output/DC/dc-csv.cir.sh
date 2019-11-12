de62fa58379b49f1bfb1f1a93d4e9a0ff7fc4b5d
Pete Sholander <peshola@sandia.gov>
2019-11-12 07:52:19 -0700

    Restore Valgrind Testing for Two FORMAT=<CSV|NOINDEX> Tests
    Yesterday's change of comparator (to file_compare.pl) broke
    Valgrind testing for these two tests.  This commit fixes that
    omission.
    
    SON Bug 985
    http://joseki-vm.sandia.gov/bugzilla/show_bug.cgi?id=985

--- Netlists/Output/DC/dc-csv.cir.sh
+++ Netlists/Output/DC/dc-csv.cir.sh
@@ -55,6 +55,24 @@ if ( !(-f "$CIRFILE.csv")) {
 
 if (defined ($xyceexit)) {print "Exit code = $xyceexit\n"; exit $xyceexit;}
 
+# If this is a VALGRIND run, we don't do our normal verification, we
+# merely run "valgrind_check.sh" as if it were xyce_verify.pl
+if ($XYCE_VERIFY =~ m/valgrind_check/)
+{
+    print STDERR "DOING VALGRIND RUN INSTEAD OF REAL RUN!";
+    if (system("$XYCE_VERIFY $CIRFILE $GOLDCSV $CIRFILE.prn > $CIRFILE.prn.out 2>&1 $CIRFILE.prn.err"))
+    {
+        print "Exit code = 2 \n";
+        exit 2;
+    }
+    else
+    {
+        print "Exit code = 0 \n";
+        exit 0;
+    }
+}
+
+# compare gold and test files
 $retcode = 0;
 
 $absTol=1e-5;
