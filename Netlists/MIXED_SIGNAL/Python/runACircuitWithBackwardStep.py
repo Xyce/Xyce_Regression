2d0f1fed897d04f12140ad1f103ccb6a389311c1
Pete Sholander <peshola@sandia.gov>
2020-02-12 10:54:49 -0700

    Test Backward Time Step for Mixed Signal Interface
    This commit adds a test for what happens when a "backward step" is
    requested with a simulateUntil() call by the Mixed Signal Interface.
    In that case, a warning message is emitted and the simulation then
    runs to completion.
    
    SON Bug 1075
    http://joseki-vm.sandia.gov/bugzilla/show_bug.cgi?id=1075

---
+++ Netlists/MIXED_SIGNAL/Python/runACircuitWithBackwardStep.py
@@ -0,0 +1,43 @@
+# This file is used to test the Python simulateUntil() method
+# with a backward time step.  This will cause the simulation to
+# emit a warning mesange and then run to completion, after that
+# particular simulateUntil() call.
+
+import sys
+
+# import the Xyce libraries
+from xyce_interface import xyce_interface
+
+# this calls the xyce_interface.open() method to
+# make a xyce object
+if (len(sys.argv) > 1):
+  libDirectory = sys.argv[1]
+  xyceObj = xyce_interface(libdir=libDirectory)
+else:
+  xyceObj = xyce_interface()
+print( xyceObj )
+
+argv= ['runACircuitWithBackwardStep.cir']
+print( "calling initialize with, %s" % argv[0] )
+
+result = xyceObj.initialize(argv)
+print( "return value from initialize is %d" % result )
+
+steps = range(0,5)
+for i in steps:
+  requested_time = 0.0 + (i+1) * 0.1
+  print( "Calling simulateUntil for requested_time = %f" % requested_time )
+  actual_time = 0.0
+  (result, actual_time) = xyceObj.simulateUntil( requested_time )
+  print( "simulateUntil status = %d and actual_time = %f" % (result, actual_time) )
+  if (result == 0):
+    break
+
+# Now step backwards, and see what happens.  The simulation should emit
+# a warning message and then run to completion.
+requested_time = requested_time - 0.1
+print( "Calling simulateUntil for backwards requested_time = %f" % requested_time )
+(result, actual_time) = xyceObj.simulateUntil( requested_time )
+
+print( "calling close")
+xyceObj.close()
