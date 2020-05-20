3b20bfa46f065da2238aa8754061d5f37af41c9b
Pete Sholander <peshola@sandia.gov>
2020-05-20 13:17:12 -0600

    Add .STEP Tests for .MEASURE for .NOISE Analyses
    This commit adds tests for .MEASURE NOISE with .STEP for the AVG,
    ERR, ERR1, ERR2, EQN, INTEG, MAX, MIN, PP and RMS measures. These
    tests only cover the V() and I() operators along with the "control
    flow" for the relevant qualifiers like FROM and TO.
    
    SON Bug 1301
    http://joseki-vm.sandia.gov/bugzilla/show_bug.cgi?id=1301

---
+++ Netlists/MEASURE_NOISE/STEP/RMSTestNoise.cir.sh
@@ -0,0 +1,172 @@
+#!/usr/bin/env perl
+
+use XyceRegression::Tools;
+use MeasureCommon;
+
+$Tools = XyceRegression::Tools->new();
+#$Tools->setDebug(1);
+#$Tools->setVerbose(1);
+
+if (defined($verbose)) { $Tools->setVerbose(1); }
+
+# The input arguments to this script are:
+# $ARGV[0] = location of Xyce binary
+# $ARGV[1] = location of xyce_verify.pl script
+# $ARGV[2] = location of compare script
+# $ARGV[3] = location of circuit file to test
+# $ARGV[4] = location of gold standard prn file
+
+# If Xyce does not produce a prn file, then we return exit code 10.
+# If Xyce succeeds, but the test fails, then we return exit code 2.
+# If the shell script fails for some reason, then we return exit code 1.
+# Otherwise we return the exit code of compare or xyce_verify.pl
+
+# Since the shell script runs Xyce and the comparison program, it is
+# responsible for capturing any error output from Xyce and the STDOUT & STDERR
+# from the comparison program.  The outside script, run_xyce_regression,
+# expects the STDERR output from Xyce to go into $CIRFILE.err, the STDOUT
+# output from comparison to go into $CIRFILE.prn.out and the STDERR output from
+# comparison to go into $CIRFILE.prn.err.
+
+use Getopt::Long;
+use File::Basename;
+use File::Copy;
+
+&GetOptions( "verbose!" => \$verbose );
+$XYCE=$ARGV[0];
+$XYCE_VERIFY=$ARGV[1];
+#$XYCE_COMPARE=$ARGV[2];
+$CIRFILE=$ARGV[3];
+$GOLDPRN=$ARGV[4];
+
+#
+# Steps common to all of the measure tests are in the Perl module
+# MeasureCommon.pm.  This file assumes the analysis type was .noise
+#
+MeasureCommon::checkNoiseFilesExist($XYCE,$CIRFILE);
+
+# The next three blocks of code are used to compare the .MEASURE output
+# to stdout to the "gold" stdout in the $GSFILE (RMSTestNoiseGSfile).  This
+# output contains the information for both steps.
+
+# check that .out file exists, and open it if it does
+if (not -s "$CIRFILE.out" )
+{
+  print "Exit code = 17\n";
+  exit 17;
+}
+else
+{
+  open(NETLIST, "$CIRFILE.out");
+  open(ERRMSG,">$CIRFILE.errmsg") or die $!;
+}
+
+# Parse the .out file to find the text related to .MEASURE for all steps.
+# The measure output in the stdout text for the first step is bracketed by
+# lines that contain "Measure Functions" and "Beginning DC Operating Point
+# Calculation".  The measure output in the stdout text for the second step
+# is bracketed by lines that contain "Measure Functions" and "Solution Summary".
+# The code has not been tested for more than two steps, and may not work in that
+# case.
+my $foundStart=0;
+my $foundEnd=0;
+my @outLine;
+my $lineCount=0;
+while( $line=<NETLIST> )
+{
+  if ($line =~ /Measure Functions/)
+  {
+    $foundStart = 1;
+    $foundEnd = 0;
+  }
+
+  if ($foundStart > 0 && $line =~ /LASTMEASURE/ )
+  {
+    print ERRMSG $line;
+    #print "Found last measure line\n";
+    $foundEnd = 1;
+    $foundStart = 0;
+  }
+  elsif ( ($foundStart > 0 && $line =~ /Solution Summary/) ||
+       ($foundStart > 0 && $line =~ /Beginning DC Operating Point Calculation/) )
+  {
+    $foundEnd = 1;
+  }
+
+  if ($foundStart > 0 && $foundEnd < 1)
+  {
+    print ERRMSG $line;
+  }
+}
+
+close(NETLIST);
+close(ERRMSG);
+
+# test that the values and strings in the files $CIRFILE.errmsg and $GSFILE, for all steps,
+# match to the required tolerances
+$GSFILE="RMSTestNoiseGSfile";
+$absTol=1e-5;
+$relTol=1e-3;
+$zeroTol=1e-10;
+
+my $dirname = dirname($XYCE_VERIFY);
+my $fc = "$dirname/file_compare.pl";
+#print "$fc $CIRFILE.errmsg $GSFILE $absTol $relTol $zeroTol\n";
+`$fc $CIRFILE.errmsg $GSFILE $absTol $relTol $zeroTol > $CIRFILE.errmsg.out 2> $CIRFILE.errmsg.err`;
+my $retval=$? >> 8;
+
+if ( $retval != 0 )
+{
+  print "test Failed comparison of stdout vs. GSfile!\n";
+  print "Exit code = $retval\n";
+  exit $retval;
+}
+else
+{
+  print "Passed comparison of stdout info\n";
+}
+
+###################################################
+# process each step.  Number of steps is hard-coded
+###################################################
+$numSteps=2;
+foreach $stepNum (1 .. $numSteps)
+{
+  print "Processing data for step $stepNum\n";
+
+  # The next two blocks of code are used to compare the measured .maX file
+  # with the "Gold" .maX file, which is in OutputData/MEASURE_NOISE/STEP
+  # Check that the Gold .maX file exists
+  $maSuffix=$stepNum-1;
+  $avgString= "ma$maSuffix";
+  $GOLDAVG = $GOLDPRN;
+  $GOLDAVG =~ s/prn$/$avgString/;
+  #print "GOLDAVG file = $GOLDAVG\n";
+  $maSuffix = $stepNum-1;
+  if (not -s "$GOLDAVG" )
+  {
+    print "GOLD $avgString file does not exist\n";
+    print "Exit code = 17\n";
+    exit 17;
+  }
+
+  # compare gold and measured .ma files, for this step.
+  $MEASUREAVG = "$CIRFILE.ma$maSuffix";
+  #print "$fc $MEASUREAVG $GOLDAVG $absTol $relTol $zeroTol\n";
+  `$fc $MEASUREAVG $GOLDAVG $absTol $relTol $zeroTol > $CIRFILE.errmsg.out 2> $CIRFILE.errmsg.err`;
+  my $retval=$? >> 8;
+
+  if ( $retval != 0 )
+  {
+    print "test Failed comparison of Gold and measured .ma$maSuffix files!\n";
+    print "Exit code = $retval\n";
+    exit $retval;
+  }
+  else
+  {
+    print "Passed comparison of .ma$maSuffix files\n";
+  }
+}
+
+print "Exit code = $retval\n";
+exit $retval;
