1883e1c5ab21f970366d8046c7593e8f15aa6ab1
Eric Keiter <erkeite@sandia.gov>
2020-07-16 11:30:35 -0600

    Updated for new expression library

--- Netlists/Certification_Tests/BUG_718_SON/invalidNodes.cir.sh
+++ Netlists/Certification_Tests/BUG_718_SON/invalidNodes.cir.sh
@@ -20,21 +20,20 @@ $GOLDPRN=$ARGV[4];
 # Note that parens are escaped with \\ 
 @searchstrings = ("Netlist error: Function or variable V\\(BOGO1\\) is not defined",
                   "Netlist error: Function or variable V\\(BOGO2\\) is not defined",
-                  "Netlist error: Function or variable V\\(GND\\) is not defined",
                   "Netlist error: Function or variable V\\(BOGO3\\) is not defined",
                   "Netlist error: Function or variable V\\(BOGO4\\) is not defined",
                   "Netlist error: Function or variable V\\(BOGO5\\) is not defined",
                   "Netlist error: Function or variable N\\(BOGO6\\) is not defined",
-                  "Netlist error: Function or variable VR\\(BOGO7\\) is not defined",
-                  "Netlist error: Function or variable VI\\(BOGO8\\) is not defined",
-                  "Netlist error: Function or variable VP\\(BOGO9\\) is not defined",
-                  "Netlist error: Function or variable VM\\(BOGO9\\) is not defined",
-                  "Netlist error: Function or variable VDB\\(BOGO10\\) is not defined",
-                  "Netlist error: Function or variable VR\\(A,BOGO11\\) is not defined",
-                  "Netlist error: Function or variable VI\\(BOGO12,A\\) is not defined",
-                  "Netlist error: Function or variable VP\\(B,BOGO13\\) is not defined",
-                  "Netlist error: Function or variable VM\\(BOGO14,B\\) is not defined",
-                  "Netlist error: Function or variable VDB\\(A,BOGO15\\) is not defined"
+                  "Netlist error: Function or variable V\\(BOGO7\\) is not defined",
+                  "Netlist error: Function or variable V\\(BOGO8\\) is not defined",
+                  "Netlist error: Function or variable V\\(BOGO9\\) is not defined",
+                  "Netlist error: Function or variable V\\(BOGO9\\) is not defined",
+                  "Netlist error: Function or variable V\\(BOGO10\\) is not defined",
+                  "Netlist error: Function or variable V\\(BOGO11\\) is not defined",
+                  "Netlist error: Function or variable V\\(BOGO12\\) is not defined",
+                  "Netlist error: Function or variable V\\(BOGO13\\) is not defined",
+                  "Netlist error: Function or variable V\\(BOGO14\\) is not defined",
+                  "Netlist error: Function or variable V\\(BOGO15\\) is not defined"
 );
 
 $retval = $Tools->runAndCheckError($CIRFILE,$XYCE,@searchstrings);
