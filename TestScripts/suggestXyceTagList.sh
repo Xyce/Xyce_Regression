a89b6820b55bb83b2ce38c895b94b0959a157516
Tom Russo <tvrusso@sandia.gov>
2020-09-24 12:05:24 -0600

    Add a script to suggest tags lists for a binary
    The full Xyce test suite, even using "run_xyce_regressionMP", can take
    a long time to run.  It is common during development for developers to
    run abbreviated test runs with restricted tags lists that they think
    capture all the tests impacted by their development.
    
    It is also common for developers to forget to run the *entire* suite
    after they are done hacking, and then find that their restricted test
    set missed some tests that they broke.
    
    And even when developers think to run the full suite, they do not
    always use the same tags list that Jenkins selects, and then they wind
    up missing tests.
    
    This commit adds a script to the test suite that allows developers to
    find out what tags lists Jenkins would have used to test the binary
    they are running.  Jenkins runs "Xyce -capabilities" and parses out
    the response, generating a set of tags that run tests for all the
    capabilities reported.  In this way a developer can run a full
    Jenkins-equivalent test run *before* committing and pushing.  I highly
    recommend doing so, especially after long hacking runs.
    
    In the case of limited builds (XyceNF or Xyce-opensource), Jenkins
    tests not only that the build passes all tests supported by the
    binary, but that it FAILS all tests that use features unsupported by
    the binary.
    
    This script is run as follows:
    
       suggestXyceTagList.sh [options] <binary>
    
    where options may be "-w" to select weekly tests instead of nightly,
    or "-a" to select both weekly and nightly.  The last argument should
    be the path to a binary of Xyce that will be tested.
    
    The output of the script is a set of lines of the form:
    
      <TAGLIST variable>=<tag list>
    
    containing the tags appropriate for the most comprehensive testing of
    that binary.
    
    For a full XyceRad serial build, there is only one line output,
    TAGLIST.
    
    For a parallel XyceRad build you get two lines, "TAGLIST" and
    "TAGLIST_KLU".  Jenkins runs the TAGLIST with the normal command line
    to invoke Xyce in parallel, and TAGLIST_KLU with the additional
    "-linsol klu" command line option.
    
    For more restricted builds such as Xyce opensource, additional
    "TAGLIST_xxx_SHOULDFAIL" lines are output.  If these tag lists are
    used, everything run should fail.
    
    One could conceivably run
    
       eval `suggestXyceTagList.sh [options] <binary>`
    
    which will cause the shell to import all the tags lists as shell
    variables that can be used when invoking run_xyce_regression, e.g.:
    
       eval `suggestXyceTagList.sh /path/to/Xyce`
       run_xyce_regression ... -taglist="$TAGLIST" ... /path/to/Xyce
    
    with additional runs of run_xyce_regression to test the other tags
    lists if they are not empty.
    
    I have so far only tested this script with a serial binary, but I
    think it is correct for parallel, too.  I will test it out when my
    OpenMPI build completes.

---
+++ TestScripts/suggestXyceTagList.sh
@@ -0,0 +1,199 @@
+#!/bin/sh
+
+# First argument should be a path to a Xyce binary
+# The capabilities of this binary will be queried and we will emit
+# a suggested tags list appropriate for the most comprehensive testing of that
+# binary
+
+# one may also add a "-w" (for weekly) option to generate tags to run longer
+# tests
+
+usage()
+{
+    echo "Usage:  `basename $0` [options] <xyce binary path>"
+    echo "  -w selects longer (weekly) tests"
+    echo "  -a selects both longer (weekly) tests and shorter (nightly)"
+    return
+}
+
+if [ $# -lt 1 ]
+then
+    usage
+    exit 1
+fi
+
+TESTSET="+nightly"
+while getopts wa option
+do
+    case "$option" in
+        w) TESTSET="+weekly";;
+        a) TESTSET="?nightly?weekly";; 
+        [?]) usage
+             exit 1;
+             ;;
+    esac
+done
+shift `echo $OPTIND-1 | bc`
+
+XYCE_BINARY=$1
+TMP_CAPABILITIES_FILE=/tmp/Xyce_capabilities.$$
+# Get capabilities
+$XYCE_BINARY -capabilities > $TMP_CAPABILITIES_FILE 2>&1
+
+TAGLIST="$TESTSET"
+TAGLIST_NONFREE_SHOULDFAIL=""
+TAGLIST_RAD_SHOULDFAIL=""
+
+# Check serial or parallel:
+grep 'Parallel with MPI' $TMP_CAPABILITIES_FILE >/dev/null 2>&1
+if [ $? = 0 ]
+then
+    # For parallel runs we'll later have klu/noklu versions of each tags list
+    TAGLIST="${TAGLIST}+parallel"
+    PARALLEL=1
+else
+    # for serial runs, always add "?klu"
+    TAGLIST="${TAGLIST}+serial?klu"
+    PARALLEL=0
+fi
+
+# Verbosity
+grep 'Verbose' $TMP_CAPABILITIES_FILE>/dev/null 2>&1
+if [ $? = 0 ]
+then
+    TAGLIST="${TAGLIST}?verbose-noverbose"
+else
+    TAGLIST="${TAGLIST}-verbose?noverbose"
+fi
+
+# FFT 
+grep 'FFT' $TMP_CAPABILITIES_FILE>/dev/null 2>&1
+if [ $? = 0 ]
+then
+    TAGLIST="${TAGLIST}?fft"
+fi
+#Now tests that depend on specific compiler usage (C++11, 14, etc.)
+grep 'C++11' $TMP_CAPABILITIES_FILE>/dev/null 2>&1
+if [ $? = 0 ]
+then
+    TAGLIST="${TAGLIST}?cxx11"
+fi
+grep 'C++14' $TMP_CAPABILITIES_FILE>/dev/null 2>&1
+if [ $? = 0 ]
+then
+    TAGLIST="${TAGLIST}?cxx14"
+fi
+grep 'Stokhos enabled' $TMP_CAPABILITIES_FILE>/dev/null 2>&1
+if [ $? = 0 ]
+then
+    TAGLIST="${TAGLIST}?stokhos"
+fi
+
+#SPECIAL CASE:  For Dakota builds, we ONLY run Dakota tests, so use "+" here.
+grep 'Dakota' $TMP_CAPABILITIES_FILE>/dev/null 2>&1
+if [ $? = 0 ]
+then
+    TAGLIST="${TAGLIST}+dakota"
+fi
+
+# XDM tests are only run if we can find the xdm_bdl program in our path
+xdm_bdl -h > /dev/null 2>&1
+if test $? = 0
+then
+    TAGLIST="${TAGLIST}?xdm"
+fi
+
+# Tags that depend on what sort of build we are (full, norad, opensource)
+# should be set last, because they work by creating a second tags list
+ # based on what we've done so far.  If we do these earlier, some tests
+# can fall through the cracks.
+# NonFree models
+BUILD_IS_OS=0
+grep 'Non-Free device models' $TMP_CAPABILITIES_FILE>/dev/null 2>&1
+if [ $? = 0 ]
+then
+    TAGLIST="${TAGLIST}?nonfree"
+else
+    # Tests that should fail
+    TAGLIST_NONFREE_SHOULDFAIL="${TAGLIST}+nonfree"
+    # Tests that should pass
+    TAGLIST="${TAGLIST}-nonfree"
+    BUILD_IS_OS=1
+fi
+
+# SandiaModels
+BUILD_IS_UUR=0
+grep 'Radiation models' $TMP_CAPABILITIES_FILE>/dev/null 2>&1
+if [ $? = 0 ]
+then
+    TAGLIST="${TAGLIST}?rad"
+    # Grumble.  Weekly "Customer Circuits" tests are only run on full builds
+    if [ "${TESTSET}" = "Weekly" ]
+    then
+        TAGLIST="${TAGLIST}?customercircuits"
+    fi
+else
+    TAGLIST_RAD_SHOULDFAIL="${TAGLIST}+rad"
+    TAGLIST="${TAGLIST}-rad"
+    BUILD_IS_UUR=1
+fi
+
+# ATHENA
+grep 'ATHENA' $TMP_CAPABILITIES_FILE>/dev/null 2>&1
+if [ $? = 0 ]
+then
+    TAGLIST="${TAGLIST}?athena"
+else
+    # Can't have athena without rad, so that's already taken care of
+    TAGLIST_RAD_SHOULDFAIL="${TAGLIST_RAD_SHOULDFAIL}?athena"
+    TAGLIST="${TAGLIST}-athena"
+fi
+
+# Tests tagged as "required:qaspr" all require the reaction parser,
+# but also are all tagged as "required:rad", so must be handled
+# specially
+grep 'Reaction parser' $TMP_CAPABILITIES_FILE>/dev/null 2>&1
+if [ $? = 0 ]
+then
+    if [ $BUILD_IS_UUR -eq 0 ]
+    then
+        TAGLIST="${TAGLIST}?qaspr"
+    else
+        TAGLIST_RAD_SHOULDFAIL="${TAGLIST_RAD_SHOULDFAIL}?qaspr"
+        TAGLIST="${TAGLIST}-qaspr"
+    fi
+fi
+
+#Now output the tags lists
+if [ $PARALLEL = 1 ]
+then
+    echo "TAGLIST=${TAGLIST}-klu"
+else
+    echo "TAGLIST=${TAGLIST}"
+fi
+
+if [ "x${TAGLIST_NONFREE_SHOULDFAIL}" != "x" ]
+then
+    echo "TAGLIST_NONFREE_SHOULDFAIL=${TAGLIST_NONFREE_SHOULDFAIL}"
+fi
+if [ "x${TAGLIST_RAD_SHOULDFAIL}" != "x" ]
+then
+    echo "TAGLIST_RAD_SHOULDFAIL=${TAGLIST_RAD_SHOULDFAIL}"
+fi
+
+# Now emit +klu versions for parallel:
+if [ $PARALLEL = 1 ]
+then
+    echo "TAGLIST_KLU=${TAGLIST}+klu"
+
+    if [ "x${TAGLIST_NONFREE_SHOULDFAIL}" != "x" ]
+    then
+        echo "TAGLIST_NONFREE_SHOULDFAIL_KLU=${TAGLIST_NONFREE_SHOULDFAIL}+klu"
+    fi
+    if [ "x${TAGLIST_RAD_SHOULDFAIL}" != "x" ]
+    then
+        echo "TAGLIST_RAD_SHOULDFAIL_KLU=${TAGLIST_RAD_SHOULDFAIL}+klu"
+    fi
+
+fi
+rm -f $TMP_CAPABILITIES_FILE
