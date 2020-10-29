0a39e0b87b39869bbd3a158157be09d28316b36a
Tom Russo <tvrusso@sandia.gov>
2020-10-29 13:16:42 -0600

    Make suggestXyceTagList exclude admssens consistently
    suggestXyceTagList figures out what tags to use based on the output of
    "Xyce -capabilities", which in turn is based on preprocessor symbols
    defined by the configuration process.
    
    "--disable-admsmodels" and "--disable-adms_sensitivities" are
    independently selectable, but the latter does absolutely nothing if
    the former is already given.
    
    "Xyce -capabilities" outputs that ADMS sensitivities are compiled in
    even when ADMS devices don't exist.  I'm going to fix that, too, but
    for now I'm making suggestXyceTagList not even look for it if it knows
    ADMS devices aren't there.  Thus "-adms" always implies
    "-adms-admssens", but "?adms" could have either "?admssens" or
    "-admssens" depending on how Xyce is compiled.

--- TestScripts/suggestXyceTagList.sh
+++ TestScripts/suggestXyceTagList.sh
@@ -114,20 +114,21 @@ grep 'Verilog-A' $TMP_CAPABILITIES_FILE>/dev/null 2>&1
 if [ $? = 0 ]
 then
     TAGLIST="${TAGLIST}?adms"
+
+     # Analytic sensitivity code can be slow to compile and takes a lot of
+     # memory (or at least it used to).  Some users on tiny machines might turn
+     # it off.
+     grep 'sensitivities in ADMS' $TMP_CAPABILITIES_FILE>/dev/null 2>&1
+     if [ $? = 0 ]
+     then
+         TAGLIST="${TAGLIST}?admssens"
+     else
+         TAGLIST="${TAGLIST}-admssens"
+     fi
 else
-    TAGLIST="${TAGLIST}-adms"
+    TAGLIST="${TAGLIST}-adms-admssens"
 fi
 
-# Analytic sensitivity code can be slow to compile and takes a lot of
-# memory (or at least it used to).  Some users on tiny machines might turn
-# it off.
-grep 'sensitivities in ADMS' $TMP_CAPABILITIES_FILE>/dev/null 2>&1
-if [ $? = 0 ]
-then
-    TAGLIST="${TAGLIST}?admssens"
-else
-    TAGLIST="${TAGLIST}-admssens"
-fi
 
 # NonFree models
 BUILD_IS_OS=0
