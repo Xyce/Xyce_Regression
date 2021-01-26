6556e525f19bc7cf9252eeb1da575e1bfcd193c0
Pete Sholander <peshola@sandia.gov>
2021-01-26 11:14:59 -0700

    Add Test for FIND-AT Measure for .MEASURE FFT
    This commit adds a test for the FIND-AT measure for .MEASURE FFT.
    It covers all of the V and I operators, and a VM(1,2) voltage
    difference operator.  It also adds tests for various invalid
    .MEASURE FFT lines.
    
    SON Bug 1280
    https://joseki-srn.sandia.gov/bugzilla/show_bug.cgi?id=1280

---
+++ Netlists/MEASURE_FFT/bad_dot_measure_line1.cir.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env perl
+
+use XyceRegression::Tools;
+$Tools = XyceRegression::Tools->new();
+
+# The input arguments to this script are:
+# $ARGV[0] = location of Xyce binary
+# $ARGV[1] = location of xyce_verify.pl script
+# $ARGV[2] = location of compare script  (This is NEVER used and can be ignored)
+# $ARGV[3] = location of circuit file to test
+# $ARGV[4] = location of gold standard prn file
+
+$XYCE=$ARGV[0];
+$XYCE_VERIFY=$ARGV[1];
+$CIRFILE=$ARGV[3];
+$GOLDPRN=$ARGV[4];
+
+# check various error cases
+# These strings should be in the output of this failed Xyce run.
+# Note that ( ) and : characters must be escaped with \\
+@searchstrings = ("Netlist error: Complex operators such as VR not allowed for output variable",
+   "for ENOB measure M1 for FFT measure mode",
+   "Netlist error: Complex operators such as II not allowed for output variable",
+   "for SFDR measure M2 for FFT measure mode",
+   "Netlist error: Complex operators such as VM not allowed for output variable",
+   "for SNDR measure M3 for FFT measure mode",
+   "Netlist error: Complex operators such as IP not allowed for output variable",
+   "for THD measure M4 for FFT measure mode",
+   "Netlist error: Complex operators such as VDB not allowed for output variable",
+   "for THD measure M5 for FFT measure mode",
+   "Netlist error: Expressions not allowed for output variable for FIND measure M6",
+   "for FFT measure mode",
+   "Netlist error: Only V and I operators allowed for output variable for FIND",
+   "measure M7 for FFT measure mode",
+   "Netlist error: Only V and I operators allowed for output variable for FIND",
+   "measure M8 for FFT measure mode"
+);
+
+$retval = $Tools->runAndCheckError($CIRFILE,$XYCE,@searchstrings);
+print "Exit code = $retval\n"; exit $retval;
