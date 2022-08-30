6cd1eddd436a615393020971ac2f98caa5f3cd30
Tom Russo <tvrusso@sandia.gov>
2022-08-29 18:02:58 -0600

    Make new test platform-independent
    The XyceRegression/Tools function "runAndCheckWarnings" makes the
    assumption that all lines of warnings it is looking for will always
    appear in the same order on every platform.  This is not true of
    device warnings that come from different netlist lines, and may not be
    true in parallel.
    
    A separate function, "runAndCheckGroupedWarnings", does not make the
    same assumption and allows you to do things like have multiple groups
    of strings to check for --- each string within a group is expected to
    appear in the order given, but the groups may appear in any order.
    This is the function I should have used in the first place.
    
    In addition to switching to that function, I've added additional
    strings to search for (such as the line that says what line of the
    netlist contains the problematic .model).
    
    Xyce/backlog/project-backlog#419

--- Netlists/BSIM4_Versioning/bsim4_test1.cir.sh
+++ Netlists/BSIM4_Versioning/bsim4_test1.cir.sh
@@ -19,15 +19,20 @@ $CIRFILE=$ARGV[3];
 
 # check various error cases
 # this string should be in the warning messages of this Xyce run  
-@searchstrings = ( "Device model PCH.2: Model card specifies BSIM4 version 4.8 which is newer",
-                   " Device model NCH.2: Model card specifies BSIM4 version 4.65 not supported by",
-                   " Device model NCH.2: Parameter TEMPEOT not valid for device of version 4.61.",
-                   "Device model NCH.1: Model card specifies BSIM4 version 4.5 which is older",
-                   " Device model NCH.1: Parameter TEMPEOT not valid for device of version 4.61."
+@searchstrings = ( ["Netlist warning in file bsim4_test1.cir at or near line 10",
+                    "Device model PCH.2: Model card specifies BSIM4 version 4.8 which is newer"],
+                   ["Netlist warning in file bsim4_test1.cir at or near line 8",
+                    " Device model NCH.2: Model card specifies BSIM4 version 4.65 not supported by"],
+                   ["Netlist warning in file bsim4_test1.cir at or near line 8",
+                    "Device model NCH.2: Parameter TEMPEOT not valid for device of version 4.61."],
+                   ["Netlist warning in file bsim4_test1.cir at or near line 7",
+                    "Device model NCH.1: Model card specifies BSIM4 version 4.5 which is older"],
+                   ["Netlist warning in file bsim4_test1.cir at or near line 7",
+                    "Device model NCH.1: Parameter TEMPEOT not valid for device of version 4.61."]
                  );
 
 print "Checking warning messages for DC measure line\n";
-$retval = $Tools->runAndCheckWarning($CIRFILE,$XYCE,@searchstrings);
+$retval = $Tools->runAndCheckGroupedWarning($CIRFILE,$XYCE,@searchstrings);
 
 if ($retval != 0)
 {
